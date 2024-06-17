40724c9e6c1d3f8e05da6b3a40ac3832a2a8a88d
2hussam <hmf4.iq@gmail.com>
2024-06-17 19:15:12 +0300

    Add prediction.py

--- prediction.py
+++ prediction.py
@@ -1,4 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO("../Vechles Tracking and license plate detection git version/Vehicles-and-License-Plate-Detection-using-YOLOv8/licensePlate_openvino_model_yolov8n")
-model.predict(source ="E:\Vechles Tracking and license plate detection git version\Vehicles-and-License-Plate-Detection-using-YOLOv8\Vehcles_4k.mp4", show = True, conf = 0.3, save=True)
+model_path = "path to your model"
+source = "path to your imag or video or put 0 to open your webcam"
+
+model = YOLO(model_path)
+model.predict(source = source, show = True, conf = 0.3, save= True)
